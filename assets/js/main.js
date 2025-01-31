db9d69bad3f09039dd84564b418b1ecd7dc58366
Mirza Mujanovic <mirza@tellonym.me>
2025-01-31 10:14:39 +0100

    update: testimonials formating

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,7 +175,7 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
-    autoplayTimeout: 11000, // 11 seconds
+    autoplayTimeout: 8000, // 8 seconds
     dots: true,
     loop: true,
     items: 1
